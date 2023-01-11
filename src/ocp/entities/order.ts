07fa0ace873f84a867a3ebfd98857c607450f616
Carlos <carlos.santana33890@gmail.com>
2023-01-10 22:55:10 -0300

    open/closed principle

---
+++ src/ocp/entities/order.ts
@@ -0,0 +1,33 @@
+import { OrderStatus } from './interfaces/order-satus';
+import { Messaging } from '../services/messaging';
+import { Persistency } from '../services/persistency';
+import { ShoppingCart } from './shopping-cart';
+
+export class Order {
+  private _orderStatus: OrderStatus = 'open';
+
+  constructor(
+    private readonly cart: ShoppingCart,
+    private readonly messaging: Messaging,
+    private readonly persistency: Persistency,
+  ) {}
+
+  get orderStatus(): OrderStatus {
+    return this._orderStatus;
+  }
+
+  checkout(): void {
+    if (this.cart.isEmpty()) {
+      console.log('Carrinho vazio.');
+      return;
+    }
+
+    this._orderStatus = 'closed';
+    this.messaging.sendMessage(
+      `Seu pedido com total de ${this.cart.totalWithDiscount()} foi recebido.`,
+    );
+
+    this.persistency.saveOrder();
+    this.cart.clear();
+  }
+}
